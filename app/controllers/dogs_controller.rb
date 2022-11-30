f71aec230d91ce4933d341c89e910c38cef51aa2
JedwardMook <jedwardmook@yahoo.com>
2022-11-30 12:00:51 -0600

    Add show action

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,4 +5,11 @@ class DogsController < ApplicationController
         render json: dogs
     end
 
+    def show
+        dog = Dog.find(params[:id])
+        render json: dog
+    rescue ActiveRecord::RecordNotFound
+        render json: "Dog not found", status: :not_found
+    end
+
 end
